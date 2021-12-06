f6a076e57b837b7ab4332d597d7511ef60738368
Helfi23 <55681352+Helfi23@users.noreply.github.com>
2021-12-06 21:06:16 +0700

    Add files via upload

---
+++ ID.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo9;
+
+/**
+ *
+ * @author Helfi Apriliyandi F
+ */
+public class ID {
+    public String nama = "Firdaos";
+    private int nip = 123456789;
+    public String jabatan = "Wakil Kepala Sekolah";    
+    
+    public void nama(){
+        System.out.println("Nama : "+nama);        
+    }
+    
+    public void nip(){
+        System.out.println("Nip : "+nip);
+    }
+    
+    public void jabatan(){
+        System.out.println("Jabatan : "+jabatan);
+    }
+}
